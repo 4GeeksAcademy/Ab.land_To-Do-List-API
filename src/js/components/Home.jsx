c778ff90ca1af81e83e33aae70dc39654b4bcbf5
Ablandaeta <ablandaeta@gmail.com>
2025-04-30 19:56:44 +0000

    v1.0

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [toDo, setToDo] = useState("");
+	const [arrayToDos, setArrayToDos] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	return (
+		<div className="container d-flex justify-content-center flex-column">
+			<h1 className="my-4 text-center " >To Do List</h1>
+			<ul className="list-group w-200 aline-center">
+				<li className="list-group-item">
+					<input className="form-control" placeholder="Add Stuff To Do" 
+					onChange={(e)=>{setToDo(e.target.value)
+						}}						
+					onKeyDown={(e)=>{if (e.key === "Enter" && toDo.trim() !== ''){				
+						setArrayToDos([...arrayToDos,toDo.trim()])
+						setToDo(""); 
+					};					
+					}}
+					value={toDo}
+					/>
+				</li>
+
+				{arrayToDos.map((arrayToDo,index,array)=>{return <li className="list-group-item d-flex justify-content-between align-items-center" key={arrayToDo+index}>
+					{arrayToDo}
+					<button className="btn btn-sm text-bg-danger" 
+					onClick={() => setArrayToDos(arrayToDos.filter((_, i) => i !== index))}
+					><i className="fa-regular fa-trash-can"></i></button>
+				</li>})
+				}
+				<li className="list-group-item list-group-item-light">{`${arrayToDos.length} items left`}</li>
+			</ul>
+
+
+
 		</div>
 	);
 };
